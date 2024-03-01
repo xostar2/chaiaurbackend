dece52f40d5c1d7a569f90f7538ecfa3baf41dcb
xostar2 <rajkishans883@gmail.com>
2024-03-01 22:46:29 +0530

    pipline

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -398,6 +398,80 @@ const updateUserCoverImage = asyncHandler( async (req,res)=>{
 
 })
 
+const getUserChannalProfile =asyncHandler(async (req,res)=>{
+       const {username} = req.params
+       if(!username?.trim()){
+        throw new ApiError(400,"usernameis missing in getuserchannal");
+       }
+
+       //User.find({username})
+
+       const channel=await User.aggregate([
+        {
+            $match:{
+                username:username?.toLowerCase()
+                 
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"channel",
+                as:"subscribers"
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"subscriber",
+                as:"subscribedTo"
+            }
+        },
+        {
+            $addFields:{
+                subscribersCount:{
+                    $size:"$subscribers"
+                },
+                channelSubscribedtoCount:{
+                    $size:"$subscribedTo"
+                },
+                isSubscribed:{
+                    $cond:{
+                        if: { $in: [req.user?._id,"$subscribers"]},
+                        then: true,
+                        else:false
+                    }
+                }
+             }
+            
+        },
+        {
+            $project:{
+                fullname:1,
+                username:1,
+                subscribersCount:1,
+                channelSubscribedtoCount:1,
+                isSubscribed:1
+
+
+
+            }
+        }
+       ])
+
+       if(channel?.length){
+        throw new ApiError(404,"channel not exist");
+       }
+
+       return res
+       .status(200)
+       .json(
+        new ApiResponse(200,channel[0],"userchannel fetched successfully")
+       )
+})
+
 
 
 export {
@@ -409,7 +483,7 @@ export {
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
-
+    updateUserCoverImage,
+    getUserChannalProfile
 
 }
