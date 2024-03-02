1f84f0245d21c511348e283e866aed448f5e9f24
xostar2 <rajkishans883@gmail.com>
2024-03-02 23:33:00 +0530

    change

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -32,7 +32,7 @@ router.route("/change-password").post(verifyJWT,changeCurrentPassword)
 router.route("/current-user").get(verifyJWT,getCurrentUser)
 router.route("/update-account").patch(verifyJWT,updateAccountDetails)
 router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
-router.route("/cover-Image").patch(verifyJWT,upload.single("/coverImage"),updateUserCoverImage)
+router.route("/cover-Image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
 router.route("/c/:username").get(verifyJWT,getUserChannalProfile)//taking from params
 router.route("/history").get(verifyJWT,getWatchhistory)
 export default router;
